71f89e7612cb7ff19a5f7ccf44b6c810bdeedf30
Tobias Røikjer <tobias.roikjer@post.au.dk>
2019-11-29 12:25:26 +0100

    Add program that creates nuc/frag tracks

---
+++ src/pipeline/track/README.md
@@ -0,0 +1,5 @@
+Makes nucleosome "tracks", i.e. a list of nucleosomes and a list of fragments
+within a start and stop position.
+
+Usage: python3 parse.py  <file> <chr> <min pos> <max pos> frags.tsv 
+       python3 parse_nuc.py  <file> <chr> <min pos> <max pos> nucs.tsv
---
+++ src/pipeline/track/parse.py
@@ -0,0 +1,25 @@
+import csv
+import sys
+
+if (len(sys.argv) != 5):
+    print("Usage: <file> <chr> <min pos> <max pos>", file=sys.stderr) 
+    sys.exit(1)
+
+chrom = sys.argv[2]
+pos1 = int(sys.argv[3])
+pos2 = int(sys.argv[4])
+hits = [0 for x in range(0, pos2-pos1)]
+
+with open(sys.argv[1]) as tsvfile:
+    tsvreader = csv.reader(tsvfile, delimiter="\t")
+    for line in tsvreader:
+        if (len(line) > 1):
+            if (chrom == line[0] and int(line[1]) >= pos1 and int(line[1]) < pos2 and int(line[2]) >= pos1 and int(line[2]) < pos2):
+                hits[int(line[2])-pos1] += 1
+                hits[int(line[1])-pos1] += 1
+
+
+for i in range(0, pos2-pos1):
+    print(f"{i+pos1}\t{hits[i]}")
+
+
