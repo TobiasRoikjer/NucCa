9646e0c4ba3931da04fad6dc63b6745a2ab9b3bc
Tobias Røikjer <tobias.roikjer@post.au.dk>
2019-11-29 12:06:08 +0100

    PyProj: proper pair is too strict

--- src/tools/pyproj/src/bam.py
+++ src/tools/pyproj/src/bam.py
@@ -19,19 +19,24 @@ class BAM:
         #maxi = 0
 
         for read in self.bam_file.fetch():
-            if not read.is_proper_pair or read.is_duplicate \
+            if not read.is_paired or read.is_duplicate \
                     or read.is_secondary or read.is_supplementary:
                 continue
 
             query_name = read.query_name
 
             if query_name not in mem:
-                mem[query_name] = read.pos
+                mem[query_name] = (read.reference_start, read.reference_end)
                 #i += 1
                 #maxi = max(maxi, i)
             else:
-                mem_pos = mem[query_name]
+                mem_start, mem_end = mem[query_name]
                 del mem[query_name]
                 #i -= 1
+                if (mem_start is None or mem_end is None or read.reference_start is None or read.reference_end is None):
+                    continue
+                start = min(read.reference_start, mem_start)
+                end = max(read.reference_end, mem_end)
 
-                yield read.reference_name, min(read.pos, mem_pos), max(read.pos, mem_pos)
+                yield read.reference_name, start, end
+
